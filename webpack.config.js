9f8dbfa4f6db1d80cec080bed6fd2d30b5fb6d81
Aaron Faanes <dafrito@gmail.com>
2022-03-03 22:40:45 -0600

    Upgrade webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const {webpackConfig, relDir} = require("./webpack.common");
 module.exports = {
   entry: {
     index: relDir("src/index.ts"),
-    demo: relDir("src/demo.ts")
+    demo: relDir("src/demo.ts"),
   },
   ...webpackConfig(false),
 };
