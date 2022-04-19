fdee4f9d659b8c307e9ab9a84eab2c479ab81852
Aaron Faanes <dafrito@gmail.com>
2022-04-19 15:59:00 -0500

    Remove redundant demo

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,7 @@ const {webpackConfig, relDir} = require("./webpack.common");
 module.exports = {
   entry: {
     index: relDir("src/index.ts"),
-    demo: relDir("src/demo.ts"),
-    blockDemo: relDir("src/blockDemo.ts"),
+    demo: relDir("src/demo.ts")
   },
   ...webpackConfig(false),
 };
