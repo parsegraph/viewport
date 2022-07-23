24a8edd6ca73707dac51b7a21a3e3d1ae7252a9f
Aaron Faanes <dafrito@gmail.com>
2022-07-23 01:14:24 -0700

    Add htmldemo

---
+++ src/htmldemo.ts
@@ -0,0 +1,66 @@
+import Navport from ".";
+
+import Direction, { DirectionNode } from "parsegraph-direction";
+
+import Block, { copyStyle, DefaultBlockPalette } from "parsegraph-block";
+import { DirectionCaret } from "parsegraph-direction";
+import Carousel, { ActionCarousel } from "parsegraph-carousel";
+import Color from "parsegraph-color";
+import render from "./render";
+import { DOMContent, DOMContentArtist } from "parsegraph-artist";
+
+const artist = new DOMContentArtist();
+
+const buildGraph = (comp: Navport) => {
+  const carousel = comp.carousel();
+  const web = comp.web();
+  const car = new DirectionCaret<Block>("u", new DefaultBlockPalette());
+
+  const root = car.root();
+
+  const dirs = [
+    Direction.FORWARD,
+    Direction.DOWNWARD,
+    Direction.INWARD,
+    Direction.UPWARD,
+    Direction.BACKWARD,
+  ];
+  for (let i = 0; i < 1; ++i) {
+    let dir = Direction.NULL;
+    while (dir === Direction.NULL || car.has(dir)) {
+      dir = dirs[Math.floor(Math.random() * dirs.length)];
+    }
+    car.spawn(dir, "b");
+    const n = car.node();
+    const val = new DOMContent(() => {
+      const c = document.createElement("div");
+      c.style.fontSize = "24px";
+      c.style.color = "green";
+      c.style.pointerEvents = "all";
+      c.innerText = "this is dom content";
+      //const c = document.createElement("img");
+      //c.src = "/favicon.ico";
+      return c;
+    });
+    val.setArtist(artist);
+    val.setNode(n as unknown as DirectionNode<DOMContent>);
+    val.setOnScheduleUpdate(() => {
+      n.layoutChanged();
+    });
+    (n as unknown as DirectionNode<DOMContent>).setValue(val);
+    car.pull(dir);
+    car.move(dir);
+  }
+  return root;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const comp = new Navport(null);
+  const root = buildGraph(comp);
+  comp.setRoot(root);
+  comp.menu().setSearchCallback((cmd: string) => {
+    alert(cmd);
+  });
+
+  render(document.getElementById("demo"), comp);
+});
